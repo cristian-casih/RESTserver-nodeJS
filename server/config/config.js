5a5284eb2a00d80361a8126968b68db71015f049
Cristian Casih <cristian.casih@autocity.com.ar>
2020-02-26 14:51:16 -0300

    RESTServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,18 @@
-process.env.PORT = process.env.PORT || 3000;
+//puerto
+process.env.PORT = process.env.PORT || 3000
+
+//Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || "dev"
+
+//Base de datos
+
+let urlDB
+
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/cafe"
+} else {
+  urlDB =
+    "mongodb+srv://cristian:96o2XwkViA8yizhv@cluster0-uqiwq.mongodb.net/cafe"
+}
+process.env.URLDB=urlDB;
--- server/server.js
+++ server/server.js
@@ -1,25 +1,27 @@
-require('./config/config');
+require("./config/config")
 
-const express = require('express');
-const moongose = require('mongoose');
+const express = require("express")
+const moongose = require("mongoose")
 
-const app = express();
+const app = express()
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser")
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(require('./routes/usuario'))
+app.use(require("./routes/usuario"))
 
-moongose.connect('mongodb://localhost:27017/cafe', (err, res) => {
-    if (err) throw err;
-    console.log('DB connect..');
-
-})
+moongose.connect(
+    process.env.URLDB,
+  { useNewUrlParser: true, useCreateIndex: true },
+  (err, res) => {
+    if (err) throw err
+    console.log("DB connect..")
+  }
+)
 
 app.listen(process.env.PORT, () => {
-    console.log("Escuchando el puerto: ", process.env.PORT);
-
-});
+  console.log("Escuchando el puerto: ", process.env.PORT)
+})
